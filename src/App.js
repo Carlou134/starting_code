063c1acb0fecd2a8443c4911700277067dc0e3b9
Carlou134 <carlouvasquez134@gmail.com>
2026-03-01 01:08:13 -0500

    Nested Path

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ import { createBrowserRouter, createRoutesFromElements, Route } from 'react-rout
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Root/>} />
+  <Route path='/' element={<Root/>}>
+    <Route index element={<HomePage/>} />
+  </Route>
 ));
 
 function App() {
   return (
     // replace below with a Router Provider
     <RouterProvider router={appRouter}/>
+
   );
 }
 
